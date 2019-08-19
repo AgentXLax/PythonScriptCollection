d0735d8e9b22bead6f0f73940fc896c6d63dd6cc
AgentXLax <49303850+AgentXLax@users.noreply.github.com>
2019-08-19 00:50:58 -0600

    Create runFileSampler.py

---
+++ KCCScripts/runFileSampler.py
@@ -0,0 +1,13 @@
+from RandomFileSampler_1.0 import FileSampler
+import datetime
+
+srcRoot = 'C:\\Users\\Nathan\\Desktop\\fileSamplerPG\\sample_root'
+dstRoot = 'C:\\Users\\Nathan\\Desktop\\fileSamplerPG\\destination_root'
+date = datetime.date.today()
+##date = datetime.date(2019,6,5)##remove '##' to manually enter date!
+
+
+print('Collecting .trp files...')
+a = FileSampler(srcRoot,dstRoot,date)
+print('.trp files found!')
+a.dump()
